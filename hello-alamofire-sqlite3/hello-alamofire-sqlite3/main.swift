5148573d41ce8e2e397ce2374f5498e23928439e
haoxin <coderhaoxin@outlook.com>
2016-05-05 22:33:40 +0800

    hello-alamofire-sqlite3 - INIT

---
+++ hello-alamofire-sqlite3/hello-alamofire-sqlite3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hello-alamofire-sqlite3
+//
+//  Created by hx on 5/5/16.
+//  Copyright © 2016 hx. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
