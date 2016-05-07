b366b7726ce4be8ee53cef3a73c850596d0a4605
haoxin <coderhaoxin@outlook.com>
2016-05-08 00:47:31 +0800

    hello-snapkit - add something

---
+++ hello-snapkit/hello-snapkit/config.swift
@@ -0,0 +1,13 @@
+//
+//  config.swift
+//  hello-snapkit
+//
+//  Created by hx on 5/7/16.
+//  Copyright © 2016 hx. All rights reserved.
+//
+
+import Foundation
+
+// style
+let colorMain = 0x00aeef
+let colorGray = 0xf3f3f3
