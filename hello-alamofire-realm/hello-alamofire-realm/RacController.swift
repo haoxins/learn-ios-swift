8c2b09b94e32bcd0fc3452f24980cc38735856db
haoxin <coderhaoxin@outlook.com>
2016-05-17 01:01:58 +0800

    add ReactiveCocoa
    closes #7

--- hello-alamofire-realm/hello-alamofire-realm/RacController.swift
+++ hello-alamofire-realm/hello-alamofire-realm/RacController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import ReactiveCocoa
 
 class RacController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let textField = UITextField()
+        textField.backgroundColor = UIColor.blueColor()
+        textField.placeholder = "search"
+        textField.frame = CGRectMake(15, 50, 300, 20)
+        view.addSubview(textField)
+
+        let searchStrings = textField.rac_textSignal()
+            .toSignalProducer()
+            .map { text in text as! String }
+            .throttle(0.5, onScheduler: QueueScheduler.mainQueueScheduler)
+            .on(event: { print ($0) })
     }
 
     override func didReceiveMemoryWarning() {
