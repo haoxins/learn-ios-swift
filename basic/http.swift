65e3a2e875514daca4226d2cb135b703e496718f
haoxin <coderhaoxin@outlook.com>
2016-05-22 17:42:14 +0800

    basic: add http

---
+++ basic/http.swift
@@ -0,0 +1,71 @@
+
+import Foundation
+
+func JSONStringify(value: AnyObject, pretty: Bool = false) -> String {
+    let options = pretty ? NSJSONWritingOptions.PrettyPrinted : NSJSONWritingOptions(rawValue: 0)
+
+    if NSJSONSerialization.isValidJSONObject(value) {
+        if let data = try? NSJSONSerialization
+            .dataWithJSONObject(value, options: options) {
+            if let str = NSString(data: data, encoding: NSUTF8StringEncoding) {
+                return str as String
+            }
+        }
+    }
+
+    return ""
+}
+
+func httpRequest(req: NSMutableURLRequest, cb: (String, String) -> Void) {
+    let session = NSURLSession.sharedSession()
+
+    let task = session.dataTaskWithRequest(req, completionHandler: {
+            data, response, error in
+
+            if error != nil {
+                cb((error!.localizedDescription) as String, "")
+            } else {
+                cb("", NSString(data: data!, encoding: NSUTF8StringEncoding) as! String)
+            }
+    })
+
+    task.resume()
+}
+
+
+// use NSURLSession
+
+// simple async GET
+let req1 = NSMutableURLRequest(URL: NSURL(string: "https://httpbin.org/get")!)
+
+httpRequest(req1) {
+    (err: String, data: String) -> Void in
+        if err != "" {
+            print(err, terminator: "")
+        } else {
+            print(data, terminator: "")
+        }
+    }
+
+// simple async PUT
+let req2 = NSMutableURLRequest(URL: NSURL(string: "https://httpbin.org/put")!)
+req2.HTTPMethod = "PUT"
+req2.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+let jsonString = JSONStringify([
+    "name": "haoxin"
+])
+
+let data: NSData = jsonString.dataUsingEncoding(NSUTF8StringEncoding)!
+req2.HTTPBody = data
+
+httpRequest(req2) {
+    (err: String, data: String) -> Void in
+    if err != "" {
+        print(err, terminator: "")
+    } else {
+        print(data, terminator: "")
+    }
+}
+
+sleep(5)
