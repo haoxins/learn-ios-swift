ad538d1be1929deb57ffa2024566de2f26c128b4
haoxin <coderhaoxin@outlook.com>
2016-08-03 20:18:11 +0800

    xnews INIT

---
+++ xnews/xnews/ViewController.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.cyan
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
