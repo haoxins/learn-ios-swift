ce03354408b5644295069619edea5f135f024e38
haoxin <coderhaoxin@outlook.com>
2016-08-03 17:19:28 +0800

    xnews - INIT

---
+++ xnews/xnews/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  xnews
+//
+//  Created by hx on 8/3/16.
+//  Copyright © 2016 hx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.cyan
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
