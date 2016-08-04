0dd3d8b84b3ecfdb1e40530aaae48ac820642c05
haoxin <coderhaoxin@outlook.com>
2016-08-04 10:46:27 +0800

    lab-ios - INIT

---
+++ lab-ios/lab-ios/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  lab-ios
+//
+//  Created by hx on 8/4/16.
+//  Copyright © 2016 hx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.cyan
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
