ab1e56e18e88bba326b1bcf98b9cab6533870e52
haoxin <coderhaoxin@outlook.com>
2016-08-10 23:36:02 +0800

    Criminallntent ++

---
+++ Criminallntent/app/src/main/java/com/haoxin/hx/criminallntent/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package com.haoxin.hx.criminallntent;
+
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by hx on 8/10/16.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
