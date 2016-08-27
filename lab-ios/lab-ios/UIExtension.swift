e6067d6b06a3c5d1a04c080b6f1c1a396f2ac0ca
haoxin <coderhaoxin@outlook.com>
2016-08-27 22:42:29 +0800

    lab-ios - UIButton extension

--- lab-ios/lab-ios/UIExtension.swift
+++ lab-ios/lab-ios/UIExtension.swift
@@ -1,2 +1,63 @@
 
 import Foundation
+import UIKit
+
+/**
+ * UIColor
+ */
+extension UIColor {
+    convenience init(rgb: Int) {
+        self.init(
+            red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgb & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
+
+/**
+ * UIButton
+ */
+extension UIButton {
+
+    var title: String! {
+        set (v) {
+            self.setTitle(v, for: .normal)
+        }
+
+        get {
+            return self.titleLabel?.text
+        }
+    }
+
+    var titleFont: UIFont! {
+        set (v) {
+            self.titleLabel?.font = v
+        }
+
+        get {
+            return self.titleLabel?.font
+        }
+    }
+
+    var color: UIColor! {
+        set (v) {
+            self.setTitleColor(v, for: .normal)
+        }
+
+        get {
+            return self.titleLabel?.textColor
+        }
+    }
+
+    var radius: CGFloat! {
+        set (v) {
+            self.layer.cornerRadius = v
+        }
+
+        get {
+            return self.layer.cornerRadius
+        }
+    }
+}
--- lab-ios/lab-ios/base/UIHelper.swift
+++ lab-ios/lab-ios/base/UIHelper.swift
@@ -2,53 +2,9 @@
 import Foundation
 import UIKit
 
-func newButton(_ opts: [String: Any]) -> UIButton {
-    
-    let btn = UIButton()
-    
-    if let title = opts["title"] as? String {
-        btn.setTitle(title, for: .normal)
-    }
-    
-    if let color = opts["color"] as? UIColor {
-        btn.setTitleColor(color, for: .normal)
-    }
-    
-    if let bgColor = opts["bgColor"] as? UIColor {
-        btn.backgroundColor = bgColor
-    }
-    
-    if let radius = opts["radius"] as? Int {
-        btn.layer.cornerRadius = CGFloat(radius)
-    }
-    
-    if let font = opts["font"] as? UIFont{
-        btn.titleLabel!.font = font
-    }
-    
-    if let frame = opts["frame"] as? CGRect {
-        btn.frame = frame
-    }
-    
-    return btn
-}
-
-    }
-}
-
-        }
-        }
-    }
-    }
-    }
-
-extension UIColor {
-    convenience init(rgb: Int) {
-        self.init(
-            red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgb & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
-    }
+func newAlert(title: String, message: String, ok: String = "OK") -> UIAlertController {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let alertAction = UIAlertAction(title: ok, style: .default, handler: nil)
+    alertController.addAction(alertAction)
+    return alertController
 }
