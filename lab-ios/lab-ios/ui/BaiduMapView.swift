1c7d0a5716449db861081f83e5eaa6878df62c17
haoxin <coderhaoxin@outlook.com>
2016-08-28 18:17:16 +0800

    lab-ios - WKWebView

--- lab-ios/lab-ios/ui/BaiduMapView.swift
+++ lab-ios/lab-ios/ui/BaiduMapView.swift
@@ -1,8 +1,9 @@
 
 import Foundation
+import WebKit
 import UIKit
 
-class BaiduMapView: UIViewController {
+class BaiduMapView: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -10,6 +11,43 @@ class BaiduMapView: UIViewController {
         view.backgroundColor = UIColor.gray
 
         view.addSubview(self.getBackBtn())
+
+        self.alert("xxoo", message: "ooooooo")
+    }
+
+    func initWXCnfig() -> WKWebViewConfiguration {
+        let contentController = WKUserContentController()
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+
+        return config
+    }
+
+    func initWXWebView() -> WKWebView {
+        let webView = WKWebView(
+            frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height),
+            configuration: self.initWXCnfig()
+        )
+
+        webView.navigationDelegate = self
+
+        let req = URLRequest(url: URL(string: "http://localhost:3000/map")!)
+        print("wk: goto page")
+        webView.load(req)
+
+        return webView
+    }
+
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        //
+    }
+
+    func getLocation() {
+        //
+    }
+
+    func alert(_ title: String, message: String) {
+        self.present(newAlert(title: title, message: message), animated: true, completion: nil)
     }
 
     func getBackBtn() -> UIButton {
