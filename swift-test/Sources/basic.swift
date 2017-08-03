2808fbb7dbed9f66cd04258217f7c63e83f46f5b
haoxin <haoxins@outlook.com>
2017-08-03 21:14:26 +0800

    rm swift-test

--- swift-test/Sources/basic.swift
+++
@@ -1,8 +0,0 @@
-
-public func add(a: Int, b: Int) -> Int {
-    return a + b
-}
-
-public func sub(a: Int, b: Int) -> Int {
-  return a - b
-}
--- swift-test/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-
-import XCTest
-@testable import helloTestSuite
-
-XCTMain([
-    testCase(basicTests.allTests),
-])
