c4596d9f832b75eb73e649e815e9edd677c5de7f
haoxin <haoxins@outlook.com>
2017-08-03 21:11:56 +0800

    rm hi-kt

--- hi-kt/Readme.md
+++
@@ -1,2 +0,0 @@
-
-https://github.com/nekocode/Kotlin-Android-Template
--- hi-kt/app/src/main/java/com/example/hx/hikt/util/RxExt.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hx.hikt.util
-
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-operator fun CompositeDisposable.plus(disposable: Disposable): CompositeDisposable {
-    add(disposable)
-    return this
-}
