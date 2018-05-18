85fc50a35e8e83da8c9d05c71f3410a457e1426d
haoxin <haoxins@icloud.com>
2018-05-18 16:25:24 +0800

    x

--- hello/hello/ViewController.swift
+++ hello/hello/ViewController.swift
@@ -35,6 +35,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
